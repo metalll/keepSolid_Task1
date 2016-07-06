ea0403d3ac03eb399f2e3aca47d3c98a2457cf5d
Lexa <metall112@bk.ru>
2016-07-06 19:50:28 +0300

    Initial Commit

---
+++ taskOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  taskOne
+//
+//  Created by NullStackDev on 06.07.16.
+//  Copyright © 2016 nullStackDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ taskOne/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  taskOne
+//
+//  Created by NullStackDev on 06.07.16.
+//  Copyright © 2016 nullStackDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
